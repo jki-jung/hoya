99954004b7c7853b01bee6b3b591773f58bb7e32
Chanho Jung <jungch@naver.com>
2022-10-10 19:15:42 +0900

    Revert "re-do new_lat_planner & more"
    This reverts commit b29818a947065e0a41e149f4eac0a15d3364700c.

--- selfdrive/controls/lib/lateral_mpc_lib/lat_mpc.py
+++ selfdrive/controls/lib/lateral_mpc_lib/lat_mpc.py
@@ -5,6 +5,7 @@ import numpy as np
 from casadi import SX, vertcat, sin, cos
 
 from common.realtime import sec_since_boot
+from selfdrive.controls.lib.drive_helpers import LAT_MPC_N as N
 from selfdrive.modeld.constants import T_IDXS
 
 if __name__ == '__main__':  # generating code
@@ -17,10 +18,8 @@ EXPORT_DIR = os.path.join(LAT_MPC_DIR, "c_generated_code")
 JSON_FILE = os.path.join(LAT_MPC_DIR, "acados_ocp_lat.json")
 X_DIM = 4
 P_DIM = 2
-N = 16
 COST_E_DIM = 3
 COST_DIM = COST_E_DIM + 1
-SPEED_OFFSET = 10.0
 MODEL_NAME = 'lat'
 ACADOS_SOLVER_TYPE = 'SQP_RTI'
 
@@ -89,13 +88,14 @@ def gen_lat_ocp():
 
   ocp.cost.yref = np.zeros((COST_DIM, ))
   ocp.cost.yref_e = np.zeros((COST_E_DIM, ))
+  # TODO hacky weights to keep behavior the same
   ocp.model.cost_y_expr = vertcat(y_ego,
-                                  ((v_ego + SPEED_OFFSET) * psi_ego),
-                                  ((v_ego + SPEED_OFFSET) * psi_rate_ego),
-                                  ((v_ego + SPEED_OFFSET) * psi_rate_ego_dot))
+                                  ((v_ego + 5.0) * psi_ego),
+                                  ((v_ego + 5.0) * psi_rate_ego),
+                                  ((v_ego + 5.0) * psi_rate_ego_dot))
   ocp.model.cost_y_expr_e = vertcat(y_ego,
-                                  ((v_ego + SPEED_OFFSET) * psi_ego),
-                                  ((v_ego + SPEED_OFFSET) * psi_rate_ego))
+                                  ((v_ego + 5.0) * psi_ego),
+                                  ((v_ego + 5.0) * psi_rate_ego))
 
   # set constraints
   ocp.constraints.constr_type = 'BGH'
@@ -144,8 +144,8 @@ class LateralMpc():
     self.solve_time = 0.0
     self.cost = 0
 
-  def set_weights(self, path_weight, heading_weight, yaw_rate_weight, yaw_accel_cost):
-    W = np.asfortranarray(np.diag([path_weight, heading_weight, yaw_rate_weight, yaw_accel_cost]))
+  def set_weights(self, path_weight, heading_weight, yaw_rate_weight):
+    W = np.asfortranarray(np.diag([path_weight, heading_weight, yaw_rate_weight, 1.0]))
     for i in range(N):
       self.solver.cost_set(i, 'W', W)
     self.solver.cost_set(N, 'W', W[:COST_E_DIM,:COST_E_DIM])
@@ -158,8 +158,8 @@ class LateralMpc():
     self.yref[:,0] = y_pts
     v_ego = p_cp[0]
     # rotation_radius = p_cp[1]
-    self.yref[:,1] = heading_pts * (v_ego + SPEED_OFFSET)
-    self.yref[:,2] = yaw_rate_pts * (v_ego + SPEED_OFFSET)
+    self.yref[:,1] = heading_pts * (v_ego+5.0)
+    self.yref[:,2] = yaw_rate_pts * (v_ego+5.0)
     for i in range(N):
       self.solver.cost_set(i, "yref", self.yref[i])
       self.solver.set(i, "p", p_cp)
